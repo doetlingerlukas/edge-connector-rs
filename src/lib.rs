7880c39da5a3ba92f58ceabba6e0a17383e29dd1
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2021-08-06 23:11:56 +0200

    Add device module.

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+use std::str;
+
+pub mod device;
+
+pub const EDGE_CONNECTOR_UDP_SOCKET_BINDING: &'static str = "127.0.0.1:5999";
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
-use std::collections::HashMap;
 use std::net::UdpSocket;
 use std::str;
+use std::collections::HashMap;
+use sysinfo::{System, SystemExt};
 
-fn main() {
+use edge_connector::*;
+
+fn main() -> std::io::Result<()> {
+  let sys = System::new_all();
+  let device = device::Device::new(sys);
+
   let client = reqwest::blocking::Client::new();
-  let socket = UdpSocket::bind("127.0.0.1:5999").expect("failed to bind socket");
+  let socket = UdpSocket::bind(EDGE_CONNECTOR_UDP_SOCKET_BINDING)?;
 
   loop {
     let mut buf = [0; 30];
-    let (amt, src) = socket.recv_from(&mut buf).expect("no data recieved");
-    let buffer = &mut buf[..amt];
+    let (amt, src) = socket.recv_from(&mut buf)?;
+    let buf = &mut buf[..amt];
 
-    let msg = str::from_utf8(buffer).unwrap();
+    let msg = str::from_utf8(buf).unwrap();
     println!("{}", msg);
 
     if src.is_ipv6() {
       continue;
     }
 
-    if msg.starts_with("apollo-available") {
+    if msg.starts_with("apollo-available") && device::Device::bound_to_instance().is_some() {
       let mut body = HashMap::new();
       body.insert("type", "faasd");
+      body.insert("arch", &device.arch);
 
       let res = client.post(format!("{}:5888", src.ip().to_string()))
         .json(&body)
