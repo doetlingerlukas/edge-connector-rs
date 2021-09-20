aa3fbb088708033f6d5627965141d6a1ad804ffd
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2021-09-20 22:54:19 +0200

    Fix UDP socket binding.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod device;
 
 pub static FAASD_AUTH_KEY: &'static str = include_str!("../faas-key");
 
-pub const EDGE_CONNECTOR_UDP_SOCKET_BINDING: &'static str = "127.0.0.1:5999";
+pub const EDGE_CONNECTOR_UDP_SOCKET_BINDING: &'static str = "0.0.0.0:5999";
 
 #[derive(strum_macros::Display)]
 pub enum Message {
